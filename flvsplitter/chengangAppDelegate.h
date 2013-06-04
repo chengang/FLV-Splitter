95eba805794f97f79a0b2734fcd685aaad557bc4
chengang <chengang@cpan.org>
2013-06-04 20:35:10 +0800

    Initial Commit

---
+++ flvsplitter/chengangAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  chengangAppDelegate.h
+//  flvsplitter
+//
+//  Created by 陈 钢 on 13-6-4.
+//  Copyright (c) 2013年 陈 钢. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface chengangAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ flvsplitter/chengangAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  chengangAppDelegate.m
+//  flvsplitter
+//
+//  Created by 陈 钢 on 13-6-4.
+//  Copyright (c) 2013年 陈 钢. All rights reserved.
+//
+
+#import "chengangAppDelegate.h"
+
+@implementation chengangAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
